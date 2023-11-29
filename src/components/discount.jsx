b9291ac16e88864be3b81f42eb22007888092e15
Shafique01 <musinguzishafiq01@gmail.com>
2023-11-29 10:18:13 +0300

    fix

--- src/components/discount.jsx
+++ src/components/discount.jsx
@@ -3,7 +3,7 @@ export const Discount = ()=>{
   return(
     <>
    <div class="relative w-screen h-[333px]">
-   <img src="/public/luis-villasmil-ITFwHdPEED0-unsplash.jpg" class="w-[100%] h-[100%]" />
+   <img src="/dist/luis-villasmil-ITFwHdPEED0-unsplash.jpg" class="w-[100%] h-[100%]" />
   <div class="absolute fixed top-0 w-[100%] h-[100%] text-center flex items-center justify-center text-white">
   <div class="text-4xl font-extralight">
   Get up to <span class="font-extrabold">90% off</span> <br/>
--- src/components/header2.jsx
+++ src/components/header2.jsx
@@ -5,7 +5,7 @@ export const Header2 = ()=>{
     <>
     <section class="flex flex-row items-center bg-white p-3 justify-between  sticky top-0 shadowEl shadow-[rgba(0, 0, 0, .2)] z-10">
     <Link to="/" class="font-bold text-[#6CBC02]">affinity games</Link>
-    <Link to="/cart"><img src="/public/cart-shopping-fast-svgrepo-com.svg"/></Link>
+    <Link to="/cart"><img src="/dist/cart-shopping-fast-svgrepo-com.svg"/></Link>
     </section>
     </>
     )
