f41d8e7362fd4078daa8df7ddf16421d1390498b
Shafique01 <musinguzishafiq01@gmail.com>
2023-11-29 14:23:38 +0300

    change image

--- src/components/discount.jsx
+++ src/components/discount.jsx
@@ -2,8 +2,7 @@
 export const Discount = ()=>{
   return(
     <>
-   <div class="relative w-screen h-[333px]">
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9svQ1bjdFdopLfD9vunbAbWGpRQYnVe3RnA&usqp=CAU" class="w-[100%] h-[100%]" />
+   <div class="relative w-screen h-[333px] bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9svQ1bjdFdopLfD9vunbAbWGpRQYnVe3RnA&usqp=CAU')] bg-cover bg-center">
   <div class="absolute fixed top-0 w-[100%] h-[100%] text-center flex items-center justify-center text-white">
   <div class="text-4xl font-extralight">
   Get up to <span class="font-extrabold">90% off</span> <br/>
