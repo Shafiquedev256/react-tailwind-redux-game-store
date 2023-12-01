ba6338b2e32ff1d78a53b8a677314a62fefbf70b
Shafique01 <musinguzishafiq01@gmail.com>
2023-12-01 21:56:22 +0300

    updates

--- src/components/header2.jsx
+++ src/components/header2.jsx
@@ -1,11 +1,12 @@
 import {Link} from "react-router-dom"
+import icon_cart from "../assets/cart-shopping-fast-svgrepo-com.svg"
 export const Header2 = ()=>{
   
   return(
     <>
     <section class="flex flex-row items-center bg-white p-3 justify-between  sticky top-0 shadowEl shadow-[rgba(0, 0, 0, .2)] z-10">
     <Link to="/" class="font-bold text-[#6CBC02]">affinity games</Link>
-    <Link to="/cart"><img src="/dist/cart-shopping-fast-svgrepo-com.svg"/></Link>
+    <Link to="/cart"><img src={icon_cart}/></Link>
     </section>
     </>
     )
